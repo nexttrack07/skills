c4decaced36876362ec3c34d291eecfd33e2f8c4
Faadhil Hakim <faadhil@nexttrack.biz>
2022-10-25 14:38:38 -0500

    initialize firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,5 +17,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+}
 export const analytics = getAnalytics(app);
